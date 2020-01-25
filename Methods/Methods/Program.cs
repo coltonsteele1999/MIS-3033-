7bd0e409fcf631a128561b24029e1a95e24bb0d7
Colton Steele <colton.steele@ou.edu>
2020-01-25 14:52:36 -0600

    Finished both methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static double LineValueForY()
+        {
+            Console.WriteLine("What is the slope of your function?");
+            double m = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your x value?");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your y-intercept?");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double y = m * x + b;
+            Console.WriteLine($"The y value of your function is {y}");
+            return y;
+        }
+        static int Factorial()
+        {
+            Console.WriteLine("What integer would you like to know the factorial of?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            int number=1;
+            for (int i = 1; i <= answer; i++)
+            {
+                number = number * i;
+            }
+            Console.WriteLine($"The factorial of your number is {number}");
+            return number;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            LineValueForY();
+
+            Console.WriteLine("---------------------");
+            Factorial();
+            Console.ReadKey();
+        }
+    }
+}
